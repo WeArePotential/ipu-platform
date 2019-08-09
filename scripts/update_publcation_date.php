f7d6d1847644da4ab0bad89d658737799213beac
Peter Mason <pmasonids@gmail.com>
2019-08-09 10:01:57 +0100

    Code for update of publication dates, plus amend to display.

---
+++ scripts/update_publcation_date.php
@@ -0,0 +1,506 @@
+<?php
+$db_nodes = [];
+
+$d8_nodes[407] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[408] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[409] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[410] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[411] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[412] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[413] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[414] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[415] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[416] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[417] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[418] = array(  "field_publication_date"=>"2004-01-01 00:00:00",
+);
+
+$d8_nodes[419] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[420] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[421] = array(  "field_publication_date"=>"2000-01-01 00:00:00",
+);
+
+$d8_nodes[422] = array(  "field_publication_date"=>"1998-01-01 00:00:00",
+);
+
+$d8_nodes[423] = array(  "field_publication_date"=>"1998-01-01 00:00:00",
+);
+
+$d8_nodes[424] = array(  "field_publication_date"=>"1997-01-01 00:00:00",
+);
+
+$d8_nodes[425] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[426] = array(  "field_publication_date"=>"1989-01-01 00:00:00",
+);
+
+$d8_nodes[427] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[428] = array(  "field_publication_date"=>"1989-01-01 00:00:00",
+);
+
+$d8_nodes[429] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[430] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[431] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[432] = array(  "field_publication_date"=>"1986-01-01 00:00:00",
+);
+
+$d8_nodes[433] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[434] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[435] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[436] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[437] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[438] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[439] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[440] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[441] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[442] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[443] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[444] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[445] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[446] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[447] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[448] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[449] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[450] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[451] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[452] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[453] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[454] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[455] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[457] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[458] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[459] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[460] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[461] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[462] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[463] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[464] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[465] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[466] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[467] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[468] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[469] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[470] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[471] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[472] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[473] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[474] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[475] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[476] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[477] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[478] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[535] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[536] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[537] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[538] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[539] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[540] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[541] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[542] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[543] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[544] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[549] = array(  "field_publication_date"=>"2005-01-01 00:00:00",
+);
+
+$d8_nodes[550] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[551] = array(  "field_publication_date"=>"2005-01-01 00:00:00",
+);
+
+$d8_nodes[553] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[554] = array(  "field_publication_date"=>"2005-01-01 00:00:00",
+);
+
+$d8_nodes[555] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[556] = array(  "field_publication_date"=>"2004-01-01 00:00:00",
+);
+
+$d8_nodes[557] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[558] = array(  "field_publication_date"=>"2004-01-01 00:00:00",
+);
+
+$d8_nodes[559] = array(  "field_publication_date"=>"2004-01-01 00:00:00",
+);
+
+$d8_nodes[560] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[561] = array(  "field_publication_date"=>"2004-01-01 00:00:00",
+);
+
+$d8_nodes[562] = array(  "field_publication_date"=>"2003-01-01 00:00:00",
+);
+
+$d8_nodes[563] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[564] = array(  "field_publication_date"=>"2003-01-01 00:00:00",
+);
+
+$d8_nodes[565] = array(  "field_publication_date"=>"2002-01-01 00:00:00",
+);
+
+$d8_nodes[566] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[567] = array(  "field_publication_date"=>"2001-01-01 00:00:00",
+);
+
+$d8_nodes[568] = array(  "field_publication_date"=>"1999-01-01 00:00:00",
+);
+
+$d8_nodes[569] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[570] = array(  "field_publication_date"=>"1999-01-01 00:00:00",
+);
+
+$d8_nodes[571] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[572] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[573] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[574] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[575] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[576] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[577] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[578] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[579] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[580] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[581] = array(  "field_publication_date"=>"2008-01-01 00:00:00",
+);
+
+$d8_nodes[582] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[583] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[584] = array(  "field_publication_date"=>"2007-01-01 00:00:00",
+);
+
+$d8_nodes[585] = array(  "field_publication_date"=>"2006-01-01 00:00:00",
+);
+
+$d8_nodes[586] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[587] = array(  "field_publication_date"=>"2005-01-01 00:00:00",
+);
+
+$d8_nodes[588] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[589] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[590] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[591] = array(  "field_publication_date"=>"2013-01-01 00:00:00",
+);
+
+$d8_nodes[592] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[593] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[594] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[595] = array(  "field_publication_date"=>"2011-01-01 00:00:00",
+);
+
+$d8_nodes[596] = array(  "field_publication_date"=>"2015-01-01 00:00:00",
+);
+
+$d8_nodes[597] = array(  "field_publication_date"=>"2010-01-01 00:00:00",
+);
+
+$d8_nodes[598] = array(  "field_publication_date"=>"2009-01-01 00:00:00",
+);
+
+$d8_nodes[599] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[600] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[601] = array(  "field_publication_date"=>"2014-01-01 00:00:00",
+);
+
+$d8_nodes[602] = array(  "field_publication_date"=>"2012-01-01 00:00:00",
+);
+
+$d8_nodes[8281] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8313] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8314] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8316] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8340] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8349] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8422] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8423] = array(  "field_publication_date"=>"2016-01-01 00:00:00",
+);
+
+$d8_nodes[8476] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[8506] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[8555] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[8611] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[8788] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[8789] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[8826] = array(  "field_publication_date"=>"2017-01-01 00:00:00",
+);
+
+$d8_nodes[9279] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9306] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9307] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9495] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9626] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9627] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9759] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9775] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9777] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9786] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$d8_nodes[9826] = array(  "field_publication_date"=>"2018-01-01 00:00:00",
+);
+
+$counter = 0;
+$nids = \Drupal::entityQuery('node')
+  //->condition('nid', 9741)
+  ->condition('type', 'publication')
+  ->execute();
+
+print ("Found ".count($nids)." event nodes\n");
+$nodes = entity_load_multiple('node', $nids, TRUE);
+
+foreach ($nodes as $node_id => $node) {
+  if (!empty($d8_nodes[$node_id])) {
+    $string = $d8_nodes[$node_id]['field_publication_date'];
+    $string = substr($string, 0, strpos($string, ' '));
+    $node->field_publication_date->value = $string; //str_ireplace(' ', 'T', $thedate);
+    $counter++;
+    $node->save();
+
+  }
+  //print("\n - Updated node $node_id \n");
+}
+
+print("\n - Updated $counter nodes");
+?>
