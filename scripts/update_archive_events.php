e9e75c99a30511bd7e1f980255513a90b0b34b3d
Peter Mason <pmasonids@gmail.com>
2019-10-18 10:34:38 +0100

    New script for archive events updates. Not working

---
+++ scripts/update_archive_events.php
@@ -0,0 +1,288 @@
+<?php
+$db_nodes = [];
+$d8_nodes[67]= array("field_caption"=>array("value"=>"&lt;p&gt;Frenchman Frédéric Passy (left) and Englishman William Randal Cremer created IPU out of a deep commitment to peace and dialogue -&amp;nbsp;© FAO&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[68]= array("field_caption"=>array("value"=>"&lt;p&gt;Frédéric Passy, born into an influential French family, became a devoted campaigner for peace.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[69]= array("field_caption"=>array("value"=>"&lt;p&gt;Sir William Randal Cremer’s work for peace was inspired by a lecture he heard as a young man.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[70]= array("field_caption"=>array("value"=>"&lt;p&gt;By Dianakc via Wikimedia Commons,&amp;nbsp;CC BY-SA 3.0&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[91]= array("field_caption"=>array("value"=>"&lt;p&gt;Moroccan Abdelwahad Radi is one in a series of distinguished former IPU Presidents © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[92]= array("field_caption"=>array("value"=>"&lt;p&gt;Anders B Johnsson oversaw a critical period of expansion and change during his 16 years at the helm @ IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[93]= array("field_caption"=>array("value"=>"&lt;p&gt;&amp;nbsp;IPU President&amp;nbsp;Gabriela Cuevas Barron. © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[98]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © Reuters/Khaled Abdallah Ali Al Mahdi&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[101]= array("field_caption"=>array("value"=>"&lt;p&gt;Martin Chungong brings a wealth of experience to his role as Secretary General © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[187]= array("field_caption"=>array("value"=>"&lt;p&gt;© Olivier Vernay&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[196]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[197]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[200]= array("field_caption"=>array("value"=>"&lt;p&gt;Parliaments have a vital role to play in ending the misery of conflicts. &lt;a href=&quot;https://www.flickr.com/photos/julien_harneis/1319014198/&quot;&gt;Photo &lt;/a&gt;by Julian Harneis/&lt;a href=&quot;http://creativecommons.org/licenses/by/2.0/&quot;&gt;CCBY&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[201]= array("field_caption"=>array("value"=>"&lt;p&gt;&lt;a href=&quot;https://www.flickr.com/photos/worldbank/1196395520/&quot;&gt;Photo &lt;/a&gt;by Ami Vitale/World Bank/&lt;a href=&quot;http://creativecommons.org/licenses/by/2.0/&quot;&gt;CCBY&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[202]= array("field_caption"=>array("value"=>"&lt;p&gt;&lt;a href=&quot;https://www.flickr.com/photos/laura_d/8013398764/&quot;&gt;Photo &lt;/a&gt;by ytulauratambien/&lt;a href=&quot;http://creativecommons.org/licenses/by/2.0/&quot;&gt;CCBY&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[203]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © Reuters/David Mercado&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[205]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[206]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[207]= array("field_caption"=>array("value"=>"&lt;p&gt;© Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[209]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[210]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © IPU/ A.Blagojevic&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[214]= array("field_caption"=>array("value"=>"&lt;p&gt;©IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[215]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[216]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[217]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[218]= array("field_caption"=>array("value"=>"&lt;p&gt;&lt;a href=&quot;https://www.flickr.com/photos/adam_jones/3774044278/&quot;&gt;Photo:&lt;/a&gt;&amp;nbsp;Adam Jones/&lt;a href=&quot;http://creativecommons.org/licenses/by/2.0/&quot;&gt;CCBY&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[219]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[220]= array("field_caption"=>array("value"=>"&lt;p&gt;Stateless refugee children.Photo: © AFP Photo / Behrouz Mehri&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[221]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[222]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[224]= array("field_caption"=>array("value"=>"&lt;p&gt;&lt;a href=&quot;https://www.flickr.com/photos/un_photo/5474157467&quot;&gt;© United Nations Photos&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[226]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[396]= array("field_caption"=>array("value"=>"&lt;p&gt;Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8350]= array("field_caption"=>array("value"=>"&lt;p&gt;World trade policies affect billions of people across the planet. &lt;a href=&quot;https://www.flickr.com/photos/iita-media-library/4606173727/&quot;&gt;&lt;span class=&quot;diffchange&quot;&gt;Photo:&lt;/span&gt;&lt;/a&gt; &lt;span lang=&quot;EN-GB&quot; style=&quot;font-size:10.0pt;line-height:
+115%;font-family:&amp;quot;Arial&amp;quot;,&amp;quot;sans-serif&amp;quot;;mso-fareast-font-family:Calibri;
+mso-ansi-language:EN-GB;mso-fareast-language:FR-CH;mso-bidi-language:AR-SA&quot;&gt;©&lt;/span&gt;&lt;span class=&quot;diffchange&quot;&gt;IITA/&lt;/span&gt;&lt;a href=&quot;http://creativecommons.org/licenses/by/2.0/&quot;&gt;&lt;span class=&quot;diffchange&quot;&gt;CCBY&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8351]= array("field_caption"=>array("value"=>"&lt;p&gt;The Hearings provide parliamentary input into the work of the United Nations. Photo: © IPU/Joel Sheakoski&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8352]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU representatives regularly address UN conferences and the General Assembly. Photo: &lt;span lang=&quot;EN-GB&quot; style=&quot;font-size:11.0pt;line-height:
+115%;font-family:&amp;quot;Calibri&amp;quot;,&amp;quot;sans-serif&amp;quot;;mso-fareast-font-family:Calibri;
+mso-bidi-font-family:&amp;quot;Times New Roman&amp;quot;;mso-ansi-language:EN-GB;mso-fareast-language:
+EN-US;mso-bidi-language:AR-SA&quot;&gt;©&amp;nbsp;&lt;/span&gt;IPU/Joel Sheakoski&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8353]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU regularly engages with UN processes. Photo:&amp;nbsp;© J. Sheakoski&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8354]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU works to bring peace and reconciliation to countries dealing with conflict. Photo: &lt;span lang=&quot;EN-GB&quot; style=&quot;font-size:11.0pt;line-height:
+115%;font-family:&amp;quot;Calibri&amp;quot;,&amp;quot;sans-serif&amp;quot;;mso-fareast-font-family:Calibri;
+mso-bidi-font-family:&amp;quot;Times New Roman&amp;quot;;mso-ansi-language:EN-GB;mso-fareast-language:
+EN-US;mso-bidi-language:AR-SA&quot;&gt;©&lt;/span&gt; Reuters/Damir Sagolj.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8358]= array("field_caption"=>array("value"=>"&lt;p&gt;The growing threat of global terrorism has only heightened the need for disarmament and non-proliferation. Photo: ©Jonathan McIntosh/CCBY&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8368]= array("field_caption"=>array("value"=>"&lt;p&gt;We pay particular attention to building parliamentary capacity in the areas of human rights and gender equality. Photo: ©Reuters/Joseph Penney&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8369]= array("field_caption"=>array("value"=>"&lt;p&gt;We help parliaments with library services, ICT, committee structures and procedural rules. Photo:&amp;nbsp;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8372]= array("field_caption"=>array("value"=>"&lt;ol&gt;
+	&lt;li&gt;Democracy is founded on the primacy of the law and the exercise of human rights. Photo: © Reuters/Omar Sobhani&lt;/li&gt;
+&lt;/ol&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8373]= array("field_caption"=>array("value"=>"&lt;p&gt;Parliaments should be representative, open, accessible, accountable and effective. Photo © United Nations/CCBY&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8374]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU&#039;s toolkits help parliaments evaluate their performance. Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8375]= array("field_caption"=>array("value"=>"&lt;p&gt;Parliamentary oversight is extremely important to democracy and to implementing the Sustainable Development Goals. Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8376]= array("field_caption"=>array("value"=>"&lt;p&gt;The digital parliament is a living entity.&amp;nbsp;©&amp;nbsp;Chamber of Deputies of Chile.&amp;nbsp;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8377]= array("field_caption"=>array("value"=>"&lt;p&gt;The Common Principles were elaborated with parliamentary and expert partners on the basis of decades of experience in technical assistance. Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8378]= array("field_caption"=>array("value"=>"&lt;p&gt;Hundreds of events have been held around the world since Democracy Day was launched in 2008.&amp;nbsp; &lt;a href=&quot;http://www.flickr.com/photos/unwomen/6443611109/&quot;&gt;Photo &lt;/a&gt;by UN/&lt;a href=&quot;http://creativecommons.org/licenses/by/2.0/&quot;&gt;CCBY&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8380]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU has been collecting data on women in politics since the 1970s, when the issue of women’s participation in decision making was barely taken into account. Photo: ©Reuters/J. Ernst&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8381]= array("field_caption"=>array("value"=>"&lt;p&gt;The responsibility for advancing gender equality in parliament has fallen too often on women’s shoulders. Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8382]= array("field_caption"=>array("value"=>"&lt;p&gt;Women MPs began to meet within the framework of IPU events in the late 1970s. Photo: ©Reuters/Z. Souissi&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8383]= array("field_caption"=>array("value"=>"&lt;p&gt;Women speakers have shaped their parliaments, both in the way they operate and in their outputs. Photo: © IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8386]= array("field_caption"=>array("value"=>"&lt;p&gt;Violence against women in parliament poses a serious challenge to democracy. Photo: © Garry Knight&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8388]= array("field_caption"=>array("value"=>"&lt;p&gt;We strengthen legislation which deals with perpetrators and protection, services and reparation for victims. ©Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8389]= array("field_caption"=>array("value"=>"&lt;p&gt;Women suffer some of the worst impacts of war, including gender-based and sexual violence. Photo: ©Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8390]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU works with the United Nations and other specialized agencies in implementing common policy objectives. Photo: ©&amp;nbsp;UN Photo/Eskinder Debebe&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8397]= array("field_caption"=>array("value"=>"&lt;p&gt;The HIV epidemic remains a huge social, human and developmental challenge. Photo: ©Reuters/Navesh Chitrakar&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8399]= array("field_caption"=>array("value"=>"&lt;p&gt;We help parliaments around the world strengthen their capacity to better serve their people. Photo: ©IPU.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8400]= array("field_caption"=>array("value"=>"&lt;p&gt;Today women, children and adolescents face unprecedented social, economic and cultural change. Photo: ©Reuters/Siphiwe Sibeko.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8401]= array("field_caption"=>array("value"=>"&lt;p&gt;Every year, MPs around the world face abuse, mistreatment or even death for simply doing their work. &lt;a href=&quot;https://www.flickr.com/photos/alisdare/27702901196/&quot;&gt;Photo&lt;/a&gt;: ©Alisdare Hickson/&lt;a href=&quot;https://creativecommons.org/licenses/by-nc/2.0/&quot;&gt;CC BY-NC 2.0&lt;/a&gt;&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8402]= array("field_caption"=>array("value"=>"&lt;p&gt;We bring together young men and women MPs from around the world to devise youth-led solutions and bring a youth perspective to global affairs. Photo: ©IPU/Lucien Fortunati.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8404]= array("field_caption"=>array("value"=>"&lt;p&gt;Through the organization of conferences, meetings and workshops, we bring together young men and women parliamentarians from around the world. Photos: ©Uniondocs CC BY-NC-SA 2.0&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8405]= array("field_caption"=>array("value"=>"&lt;p&gt;Campaigns aim to raise awareness and motivate people to take action. Photo: © david_shankbone (CC BY 2.0)&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8406]= array("field_caption"=>array("value"=>"&lt;p&gt;IPU’s research on youth participation in parliaments has produced a wealth of data. Image: ©IPU.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8407]= array("field_caption"=>array("value"=>"&lt;p&gt;The landmark UN Convention on the Rights of the Child transforms children from passive objects to active right-holders. Photo:&amp;nbsp;©NurPhoto/Alvaro&amp;nbsp;Fuente&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8408]= array("field_caption"=>array("value"=>"&lt;p&gt;We undertake needs assessment missions to evaluate the preparedness of parliaments to integrate SDGs into their legislation.&amp;nbsp; Photo: © Lollie-Pop (CC-BY)&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8409]= array("field_caption"=>array("value"=>"&lt;p&gt;We work on peace and security, taking into account the specific situation and perspectives of women and youth. Photo: © AFP/Lluis Gene&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8416]= array("field_caption"=>array("value"=>"&lt;p&gt;Climate change and natural disasters exacerbate existing threats to development, peace and security. &lt;a href=&quot;https://www.flickr.com/photos/africa-renewal/5912149891/in/photostream/&quot;&gt;Photo&lt;/a&gt; ©&lt;a href=&quot;https://www.flickr.com/photos/dragonpreneur/7138607293/&quot;&gt; &lt;/a&gt;Africa Renewal/CC BY-NC-SA 2.0&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8516]= array("field_caption"=>array("value"=>"&lt;p&gt;Parliaments have a critical role in protecting people&#039;s human rights. Photo: © Catching.Light (CC BY 2.0)&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8517]= array("field_caption"=>array("value"=>"&lt;p&gt;We work closely with the Commission on the Status of Women.&amp;nbsp;©Joel Sheakoski&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8519]= array("field_caption"=>array("value"=>"&lt;p&gt;We help parliaments to engage with CEDAW and the Committee.&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8656]= array("field_caption"=>array("value"=>"&lt;p&gt;Gender Champions at the IPU. ©&amp;nbsp;Joel Sheakoski&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8681]= array("field_caption"=>array("value"=>"&lt;p&gt;©Nick Youngson&amp;nbsp;CC BY-SA 3.0&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8685]= array("field_caption"=>array("value"=>"&lt;p&gt;© Reuters/Hamad I. Mohammed&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8689]= array("field_caption"=>array("value"=>"&lt;p&gt;© Jellybeanz (CC BY-NC 2.0)&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[8690]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU/S. Alikhan&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9143]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9199]= array("field_caption"=>array("value"=>"&lt;p&gt;© Reuters / Muhammad Hamed&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9526]= array("field_caption"=>array("value"=>"&lt;p&gt;Cambodia opposition leader Kem Sokha is one of the cases before&amp;nbsp;the IPU Committee on the Human Rights of Parliamentarians.&amp;nbsp;©&amp;nbsp;STR/AFP&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9542]= array("field_caption"=>array("value"=>"&lt;p&gt;© IPU /&amp;nbsp;Lucien Fortunati&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9575]= array("field_caption"=>array("value"=>"&lt;p&gt;© Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9599]= array("field_caption"=>array("value"=>"&lt;p&gt;The&amp;nbsp;participation pf youth in the prevention and resolution of disputes is&amp;nbsp;essential for the building of sustainable peace.© Alexandre Meneghini / Reuters&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9774]= array("field_caption"=>array("value"=>"&lt;p&gt;©&amp;nbsp;Chamber of Deputies of Chile&lt;/p&gt;
+", "format"=>"full_html"),
+);
+$d8_nodes[9818]= array("field_caption"=>array("value"=>"&lt;p&gt;© Zorig Foundation&lt;/p&gt;
+", "format"=>"full_html"),
+);
+
+$counter = 0;
+$nids = \Drupal::entityQuery('node')
+ // ->condition('nid', 67, '=')
+  ->condition('type', ['basic_page', 'page'], 'IN')
+  ->execute();
+
+print ("Found ".count($nids)." nodes\n");
+$nodes = entity_load_multiple('node', $nids, TRUE);
+
+foreach ($nodes as $node_id => $node) {
+  if (!empty($d8_nodes[$node_id])) {
+    foreach ($d8_nodes[$node_id] as $field_caption) {
+      $node->set('field_caption', ['value' => htmlspecialchars_decode($field_caption['value'], ENT_QUOTES), 'format'=>$field_caption['format']]);
+    }
+    $counter++;
+    $node->save();
+
+  }
+  //print("\n - Updated node $node_id \n");
+}
+
+print("\n - Updated $counter nodes");
+?>
