404e408c2ea2c522310b2ed899d0bb27ba7e168e
Peter Mason <pmasonids@gmail.com>
2019-11-04 13:00:59 +0000

    Fix for language checking on parliaments dropdown.

--- web/modules/custom/ipu_map/src/Form/IpuCountrySelectForm.php
+++ web/modules/custom/ipu_map/src/Form/IpuCountrySelectForm.php
@@ -30,6 +30,7 @@ class IpuCountrySelectForm extends FormBase {
 
     $i = 0;
     $language =  \Drupal::languageManager()->getCurrentLanguage()->getId();
+
     foreach ($country_terms as $country_term) {
       //if ($i++ <2) print_r($country_term->get('field_iso_code')->getValue());
       if ($country_term->hasField('field_iso_code')) {
@@ -43,45 +44,56 @@ class IpuCountrySelectForm extends FormBase {
         }
       }
     }
-    $form['isocode'] = array(
+    $form['isocode'] = [
       '#type' => 'select',
-      '#options'=> $options,
-      '#attributes' => array('class' => array('ipu-country-select')), // Add a class if required
+      '#options' => $options,
+      '#attributes' => [
+        'class' => ['ipu-country-select'],
+      ],
       '#title' => t('Parliaments'),
       '#chosen' => TRUE,
       '#required' => FALSE,
-    );
-    $form['actions']['submit'] = array(
+    ];
+
+    $stubpath = ($language == 'fr' ? '/fr/parlement' : '/parliament');
+    $form['#action'] = $stubpath;
+
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => t('Go'),
       '#button_type' => 'primary',
-    );
+    ];
 
     $block_manager = \Drupal::service('plugin.manager.block');
-    $config = ['nid'=> 236, 'link_text'=>'Our support for parliaments', 'view_mode'=>'highlight', 'current'=>FALSE];// You can hard code configuration or you load from settings.
+    $config = [
+      'nid' => 236,
+      'link_text' => 'Our support for parliaments',
+      'view_mode' => 'highlight',
+      'current' => FALSE,
+    ];
+    // You can hard code configuration or you load from settings.
     $plugin_block = $block_manager->createInstance('ipu_node_block', $config);
     $render = $plugin_block->build();
     $html = \Drupal::service('renderer')->renderRoot($render);
-    $form['#suffix'] = '<div class="parliaments-menu-cta-node">'.$html.'</div>';
+    $form['#suffix'] = '<div class="parliaments-menu-cta-node">' . $html . '</div>';
 
     return $form;
   }
+
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $language =  \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $stubpath = ($language == 'fr' ? '/parlement' : '/parliament');
+    // We use the action to build the url as the submitForm doesn't recognise
+    // the language of the calling page.
     $isocode = $form_state->getValue('isocode');
     if ($isocode) {
       if ($isocode != 'XX') {
+        $stubpath = $form['#action'];
         $path = $stubpath . '/' . $isocode;
         $form_state->setRedirectUrl(Url::fromUserInput($path));
       }
     }
-    //$response = new RedirectResponse($path);
-    //$response->send();
-    return;
-    //drupal_set_message($this->t('@emp_name ,Your application is being submitted!', array('@emp_name' => $form_state->getValue('employee_name'))));
   }
+
 }
